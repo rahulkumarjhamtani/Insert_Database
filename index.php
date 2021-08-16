287065787ed2991a2119830df4853accee07472d
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-08-16 18:26:31 +0530

    new project

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Fill Form</title>
+</head>
+
+<body>
+
+    <div class="square1">
+        
+    </div>
+
+    <div class="container">
+        <h1>Welcome to my form</h1>
+        <p>Enter your details and submit this form to confirm your participation</p>
+    </div>
+
+</body>
+
+<script src="assets/js/script.js"></script>
+
+</html>
